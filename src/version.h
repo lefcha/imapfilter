0a8f5580eddd573b5c099b6cf216fc16fe3ab397
Lefteris Chatzimparmpas <lefcha@fastmail.net>
2011-12-08 22:32:01 +0100

    Prepare release v2.4.1.

--- src/version.h
+++ src/version.h
@@ -3,7 +3,7 @@
 
 
 /* Program's version number. */
-#define VERSION		"2.4"
+#define VERSION		"2.4.1"
 
 /* Program's copyright. */
 #define COPYRIGHT	"Copyright (c) 2001-2011 Eleftherios Chatzimparmpas"
