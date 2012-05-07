1504b31ee9bbb0804cadac3f8904dff5e9a14050
Neil Smithline <github.com@neilsmithline.com>
2012-05-07 15:13:44 -0400

    added gmail account sample

---
+++ samples/gmail-account.lua
@@ -0,0 +1,26 @@
+------------------------------------------------------------------------
+-- Gmail account configuration example for imapfilter
+--
+-- 1) Insert your username and password into the appropriate fields
+-- below
+--
+-- 2) Remove the leading "--" and put the next line near the beginning of
+-- your config.lua file:
+--     dofile 'gmail-account.lua'
+------------------------------------------------------------------------
+
+gmail = IMAP {
+    server = 'imap.gmail.com',
+    port = 993,
+    username = 'your username',
+    password = 'your password',
+    ssl = 'ssl3',
+}
+
+------------------------------------------------------------------------
+-- Gmail2Gcal for IMAPFilter by Neil Smithline is licensed under a
+-- Creative Commons Attribution-ShareAlike 3.0 Unported License.
+--
+-- See https://github.com/Neil-Smithline/imapfilter-gmail2gcal for
+-- more details.
+------------------------------------------------------------------------
