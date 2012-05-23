7fa4b0533197a761707113318b4437ec7117f0a8
Neil Smithline <github.com@neilsmithline.com>
2012-05-22 23:43:31 -0400

    update comments of gmail config

--- contrib/account-configuration/gmail-account.lua
+++ contrib/account-configuration/gmail-account.lua
@@ -21,6 +21,5 @@ gmail = IMAP {
 -- Gmail Config for IMAPFilter by Neil Smithline is licensed under a
 -- Creative Commons Attribution-ShareAlike 3.0 Unported License.
 --
--- See https://github.com/Neil-Smithline/imapfilter-gmail2gcal for
--- more details.
+-- See https://github.com/Neil-Smithline/imapfilter for more details.
 ------------------------------------------------------------------------
