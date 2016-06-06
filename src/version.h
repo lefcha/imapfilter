77670d79a687c09813026911f1e1f17c4227298b
Lefteris Chatzimparmpas <lefcha@fastmail.net>
2016-06-07 00:37:30 +0300

    Prepare release v2.6.7.

--- src/version.h
+++ src/version.h
@@ -3,7 +3,7 @@
 
 
 /* Program's version number. */
-#define VERSION		"2.6.6"
+#define VERSION		"2.6.7"
 
 /* Program's copyright. */
 #define COPYRIGHT	"Copyright (c) 2001-2016 Eleftherios Chatzimparmpas"
