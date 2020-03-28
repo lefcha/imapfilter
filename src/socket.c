644b46c03ba08d0acdc034234f72bbc287a4c644
Stefan Strogin <steils@gentoo.org>
2020-03-28 06:56:07 +0200

    Fix build with LibreSSL

--- src/socket.c
+++ src/socket.c
@@ -143,7 +143,7 @@ open_secure_connection(session *ssn)
 		goto fail;
 
 	if (get_option_boolean("certificates")) {
-#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L && !defined(LIBRESSL_VERSION_NUMBER)
 		SSL_set_hostflags(ssn->sslconn,
 		    X509_CHECK_FLAG_NO_PARTIAL_WILDCARDS);
 		if (!SSL_set1_host(ssn->sslconn, ssn->server)) {
