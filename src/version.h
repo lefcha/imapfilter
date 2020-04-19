b0c60182324a091d0d3295f3ea4615d53a8be3df
Lefteris Chatzimparmpas <lefcha@fastmail.com>
2020-04-19 22:33:19 +0300

    Update copyright year to include 2020.

--- src/version.h
+++ src/version.h
@@ -6,7 +6,7 @@
 #define VERSION		"2.6.16"
 
 /* Program's copyright. */
-#define COPYRIGHT	"Copyright (c) 2001-2019 Eleftherios Chatzimparmpas"
+#define COPYRIGHT	"Copyright (c) 2001-2020 Eleftherios Chatzimparmpas"
 
 
 #endif				/* VERSION_H */
