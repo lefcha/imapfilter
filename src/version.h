f6b854bc485a1ca307ca4a87af2b175132a70f5c
Lefteris Chatzimparmpas <lefcha@fastmail.com>
2020-11-18 23:30:58 +0200

    Prepare release v2.7.4.

--- src/version.h
+++ src/version.h
@@ -3,7 +3,7 @@
 
 
 /* Program's version number. */
-#define VERSION		"2.7.3"
+#define VERSION		"2.7.4"
 
 /* Program's copyright. */
 #define COPYRIGHT	"Copyright (c) 2001-2020 Eleftherios Chatzimparmpas"
