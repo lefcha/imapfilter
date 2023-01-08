e6372b88e09a4d1c573b57270de6b40846825a8b
Lefteris Chatzimparmpas <lefcha@fastmail.com>
2023-01-08 15:43:35 +0200

    Prepare release v2.8.1

--- src/version.h
+++ src/version.h
@@ -3,7 +3,7 @@
 
 
 /* Program's version number. */
-#define VERSION		"2.8.0"
+#define VERSION		"2.8.1"
 
 /* Program's copyright. */
 #define COPYRIGHT	"Copyright (c) 2001-2023 Eleftherios Chatzimparmpas"
